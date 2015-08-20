aac32a7d8d58f61e46657cc9e573654d0c28dc32
denis.terekhin <terekhin.den@gmail.com>
2015-08-20 23:33:18 +0600

    Init Storage core

---
+++ SWStorage/SWStorage/Core/RdfStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace SWStorage.Core
+{
+    class RdfStorage
+    {
+        private static IGraph _Graph;
+
+        public RdfStorage()
+        {
+            _Graph = new Graph();
+        }
+
+        public RdfStorage(String path)
+        {
+            _Graph = new Graph();
+            FileLoader.Load(_Graph, path);
+        }
+
+    }
+}
